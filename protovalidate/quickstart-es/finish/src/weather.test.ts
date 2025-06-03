b029e7f1aebb25fb0f0980abf012fc53900202f0
Steve Ayers <sayers@buf.build>
2025-06-03 17:17:40 -0400

    Tests

--- protovalidate/quickstart-es/finish/src/weather.test.ts
+++ protovalidate/quickstart-es/finish/src/weather.test.ts
@@ -1,29 +1,71 @@
 import { expect, test } from "vitest";
 import { create } from "@bufbuild/protobuf";
-import {
-  GetWeatherRequest,
-  GetWeatherRequestSchema,
-} from "./gen/bufbuild/weather/v1/weather_service_pb.js";
+import { timestampFromDate } from "@bufbuild/protobuf/wkt";
+import { GetWeatherRequestSchema } from "./gen/bufbuild/weather/v1/weather_service_pb.js";
 import { validateWeather } from "./weather.js";
 
-test("validate method is defined", () => {
-  const msg = create(GetWeatherRequestSchema);
-
-  const result = validateWeather(msg);
-
-  console.log(result);
-
-  expect(result.kind).toEqual("valid");
-});
-
-test("another", () => {
+test("valid message", () => {
   const msg = create(GetWeatherRequestSchema, {
-    latitude: 95,
+    latitude: 45,
+    longitude: 45,
   });
 
   const result = validateWeather(msg);
 
-  console.log(result);
+  expect(result).toBeDefined();
+  expect(result.kind).toEqual("valid");
+  expect(result.violations).toBeUndefined();
+});
 
+test("latitude too high", () => {
+  const msg = create(GetWeatherRequestSchema, {
+    latitude: 91,
+    longitude: 45,
+  });
+
+  const result = validateWeather(msg);
+
+  expect(result).toBeDefined();
   expect(result.kind).toEqual("invalid");
+  expect(result.violations.length).toEqual(1);
+  expect(result.violations[0].message).toEqual(
+    "value must be greater than or equal to -90 and less than or equal to 90",
+  );
+});
+
+test("latitude too low", () => {
+  const msg = create(GetWeatherRequestSchema, {
+    latitude: -180,
+    longitude: 45,
+  });
+
+  const result = validateWeather(msg);
+
+  expect(result).toBeDefined();
+  expect(result.kind).toEqual("invalid");
+  expect(result.violations.length).toEqual(1);
+  expect(result.violations[0].message).toEqual(
+    "value must be greater than or equal to -90 and less than or equal to 90",
+  );
+});
+
+test("too far in the future", () => {
+  // Create a date 7 days in the future
+  const dt = new Date();
+  dt.setDate(dt.getDate() + 7);
+
+  const msg = create(GetWeatherRequestSchema, {
+    latitude: 45,
+    longitude: 45,
+    forecastDate: timestampFromDate(dt),
+  });
+
+  const result = validateWeather(msg);
+
+  expect(result).toBeDefined();
+  expect(result.kind).toEqual("invalid");
+  expect(result.violations.length).toEqual(1);
+  expect(result.violations[0].message).toEqual(
+    "Forecast date must be in the next 72 hours.",
+  );
 });
