5e0b52a442aba9ab865cdef6d85a5a87aff81769
joerinehart <jrinehart@buf.build>
2025-09-26 09:52:33 -0400

    Update finish states

--- bsr/quickstart/finish/client/cmd/main.go
+++ bsr/quickstart/finish/client/cmd/main.go
@@ -1,24 +1,9 @@
-// Copyright 2020-2025 Buf Technologies, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
 	tagv1 "buf.build/gen/go/xUSERNAMEx/common/protocolbuffers/go/tag/v1"
 	"buf.build/gen/go/xUSERNAMEx/invoice/connectrpc/go/invoice/v1/invoicev1connect"
 	invoicev1 "buf.build/gen/go/xUSERNAMEx/invoice/protocolbuffers/go/invoice/v1"
-	"connectrpc.com/connect"
 	"context"
 	"log"
 	"net/http"
@@ -32,7 +17,7 @@ func main() {
 
 	_, err := client.CreateInvoice(
 		context.Background(),
-		connect.NewRequest(&invoicev1.CreateInvoiceRequest{
+		&invoicev1.CreateInvoiceRequest{
 			Invoice: &invoicev1.Invoice{
 				InvoiceId:  "invoice-one",
 				CustomerId: "customer-one",
@@ -49,7 +34,7 @@ func main() {
 					"valued-customer",
 				},
 			},
-		}),
+		},
 	)
 	if err != nil {
 		log.Fatalf("error creating valid invoice: %v", err)
--- bsr/quickstart/finish/client/go.mod
+++ bsr/quickstart/finish/client/go.mod
@@ -3,10 +3,8 @@ module github.com/bufbuild/buf-examples/bsr/quickstart/client
 go 1.24.0
 
 require (
-	buf.build/gen/go/xUSERNAMEx/common/protocolbuffers/go v1.36.5-20250319033322-7607f73aaca1.1
-	buf.build/gen/go/quickstarts/bsr/connectrpc/go v1.18.1-20250319031420-b7f0cadb0629.1
-	buf.build/gen/go/quickstarts/bsr/protocolbuffers/go v1.36.5-20250319031420-b7f0cadb0629.1
-	connectrpc.com/connect v1.19.0
+	buf.build/gen/go/xUSERNAMEx/common/protocolbuffers/go v1.36.9-20250926133818-03b90bc6164f.1 // indirect
+	buf.build/gen/go/xUSERNAMEx/invoice/connectrpc/gosimple v1.19.0-20250926134036-43755cde1887.1 // indirect
+	buf.build/gen/go/xUSERNAMEx/invoice/protocolbuffers/go v1.36.9-20250926134036-43755cde1887.1 // indirect
+	google.golang.org/protobuf v1.36.9 // indirect
 )
-
-require google.golang.org/protobuf v1.36.5 // indirect
