8fc56e4af63665e968765e8e00a5eda7b7fe22a4
joerinehart <jrinehart@buf.build>
2025-09-29 10:28:30 -0400

    gosimple updates for api-access

--- bsr/api-access/example/example.go
+++ bsr/api-access/example/example.go
@@ -4,27 +4,26 @@ import (
 	"context"
 	"net/http"
 
-	"buf.build/gen/go/bufbuild/reflect/connectrpc/go/buf/reflect/v1beta1/reflectv1beta1connect"
+	reflectv1beta1connect "buf.build/gen/go/bufbuild/reflect/connectrpc/gosimple/buf/reflect/v1beta1/reflectv1beta1connect"
 	reflectv1beta1 "buf.build/gen/go/bufbuild/reflect/protocolbuffers/go/buf/reflect/v1beta1"
-	connect "connectrpc.com/connect"
 )
 
 func Example() (*reflectv1beta1.GetFileDescriptorSetResponse, error) {
-	client := reflectv1beta1connect.NewFileDescriptorSetServiceClient(
+	simpleClient := reflectv1beta1connect.NewFileDescriptorSetServiceClient(
 		http.DefaultClient,
 		"https://buf.build",
 	)
-	request := connect.NewRequest(&reflectv1beta1.GetFileDescriptorSetRequest{
+	request := &reflectv1beta1.GetFileDescriptorSetRequest{
 		Module: "buf.build/connectrpc/eliza",
-	})
+	}
 	// If you're using a private BSR, set your Authorization header to a
 	// BUF_TOKEN value.
 	//
 	// request.Header().Set("Authorization", "Bearer <BUF_TOKEN>")
-	response, err := client.GetFileDescriptorSet(context.Background(), request)
+	response, err := simpleClient.GetFileDescriptorSet(context.Background(), request)
 	if err != nil {
 		return nil, err
 	}
 
-	return response.Msg, nil
+	return response, nil
 }
--- bsr/api-access/go.mod
+++ bsr/api-access/go.mod
@@ -3,13 +3,13 @@ module github.com/bufbuild/buf-examples/bsr/api-access
 go 1.24.0
 
 require (
-	buf.build/gen/go/bufbuild/reflect/connectrpc/go v1.19.0-20240117202343-bf8f65e8876c.1
+	buf.build/gen/go/bufbuild/reflect/connectrpc/gosimple v1.19.0-20240117202343-bf8f65e8876c.1
 	buf.build/gen/go/bufbuild/reflect/protocolbuffers/go v1.36.9-20240117202343-bf8f65e8876c.1
-	connectrpc.com/connect v1.19.0
 	github.com/stretchr/testify v1.11.1
 )
 
 require (
+	connectrpc.com/connect v1.19.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
