17e041e056862ab670acb0bf37c1623ce7e3836a
Dan Hudlow <dan@hudlow.org>
2025-10-20 11:39:16 -0500

    Rework examples for predefined rules (#134)
    The  nested `additional-examples` directory has additional examples
    with changes represented in a sparse directory tree which can, in turn,
    have its own `additional-examples` directory. Running `make ci` will
    perform validations on the current directory, and recursively copy
    itself and the rest of the boilerplate into the tree of
    additional-examples directories to construct and then run validations
    for each override scenario.
    
    ---------
    
    Co-authored-by: Joe Rinehart <jrinehart@buf.build>

---
+++ protovalidate/rules-predefined/additional-examples/using-rule-values/internal/bufbuild/people/v1/suffix_test.go
@@ -0,0 +1,85 @@
+// Copyright 2020-2025 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package peoplev1
+
+import (
+	"strings"
+	"testing"
+
+	"buf.build/go/protovalidate"
+	peoplev1 "github.com/bufbuild/buf-examples/protovalidate/rules-predefined/gen/bufbuild/people/v1"
+)
+
+func TestSuffixValidation(t *testing.T) {
+	t.Parallel()
+
+	expectations := map[string]struct {
+		givenName  string
+		middleName string
+		familyName string
+		title      string
+		suffix     string
+		valid      bool
+	}{
+		"Suffix at the limit is accepted": {
+			givenName:  strings.Repeat("a", 50),
+			middleName: OMIT,
+			familyName: strings.Repeat("a", 50),
+			title:      OMIT,
+			suffix:     strings.Repeat("a", 40),
+			valid:      true,
+		},
+		"An excessively long Suffix is rejected": {
+			givenName:  strings.Repeat("a", 50),
+			middleName: OMIT,
+			familyName: strings.Repeat("a", 50),
+			title:      OMIT,
+			suffix:     strings.Repeat("a", 41),
+			valid:      false,
+		},
+	}
+
+	for testName, testCase := range expectations {
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+
+			person := &peoplev1.Person{}
+			person.SetGivenName(testCase.givenName)
+			person.SetFamilyName(testCase.familyName)
+
+			if testCase.middleName != OMIT {
+				person.SetMiddleName(testCase.middleName)
+			}
+
+			if testCase.title != OMIT {
+				person.SetTitle(testCase.title)
+			}
+
+			if testCase.suffix != OMIT {
+				person.SetSuffix(testCase.suffix)
+			}
+
+			err := protovalidate.Validate(person)
+
+			if err != nil && testCase.valid {
+				t.Fatalf("unexpected validation error received: %v", err)
+			}
+
+			if err == nil && !testCase.valid {
+				t.Fatal("expected validation error")
+			}
+		})
+	}
+}
---
+++ protovalidate/rules-predefined/go.mod
@@ -0,0 +1,20 @@
+module github.com/bufbuild/buf-examples/protovalidate/rules-predefined
+
+go 1.24.0
+
+require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.9-20250912141014-52f32327d4b0.1
+	buf.build/go/protovalidate v1.0.0
+	google.golang.org/protobuf v1.36.9
+)
+
+require (
+	cel.dev/expr v0.24.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
+	github.com/google/cel-go v0.26.1 // indirect
+	github.com/stoewer/go-strcase v1.3.1 // indirect
+	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
+)
